2584eb9656a45d5925113cf29c87c1313adf7cb3
NgAnhTuan24 <luckytme2004@gmail.com>
2026-01-26 21:34:08 +0700

    Tạo scene MainMenu chuyển động cảnh vật

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform cam;
+
+    private float offsetY;
+    private float offsetZ;
+    
+    void Start()
+    {
+        offsetY = transform.position.y;
+        offsetZ = transform.position.z;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(cam.position.x, offsetY, offsetZ);
+    }
+}
