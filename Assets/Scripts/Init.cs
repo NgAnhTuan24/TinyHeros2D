769e7c0be95e3e6a5a8c25b7afa0e58fafd30d54
NgAnhTuan24 <luckytme2004@gmail.com>
2026-01-27 21:22:36 +0700

    thêm chức năng chọn nhân vật, khởi tạo đối tượng và bắt đầu màn chơi

---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject selectedCharacter = CharacterSelect.selectedCharacter;
+        GameObject playerObject = Instantiate(selectedCharacter, transform.position, Quaternion.identity);
+        playerObject.name = "Player";
+    }
+}
