c43e2c158a40324f1fb889d73508f387ea1c49f9
NgAnhTuan24 <luckytme2004@gmail.com>
2026-01-28 21:14:35 +0700

    tạo 1 script mới xử lý animation tấn công riêng
    tích hợp hoạt ảnh tấn công cho nhân vật Pink

---
+++ Assets/Scripts/Player/ComboHits.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboHits : MonoBehaviour
+{
+    public Animator anim;
+    public int noOfClicks = 0;
+    public float maxComboDelay = 0;
+
+    private float lastClickedTime = 0;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastClickedTime = Time.time;
+            noOfClicks++;
+            if(noOfClicks == 1)
+            {
+                anim.SetBool("Attack1", true);
+            }
+            noOfClicks = Mathf.Clamp(noOfClicks, 0, 2);
+        }
+    }
+
+    public void return1()
+    {
+        if (noOfClicks >= 2)
+        {
+            anim.SetBool("Attack2", true);
+        }
+        else
+        {
+            anim.SetBool("Attack1", false);
+            noOfClicks = 0;
+        }
+    }
+
+    public void return2()
+    {
+        anim.SetBool("Attack1", false);
+        anim.SetBool("Attack2", false);
+        noOfClicks = 0;
+    }
+}
