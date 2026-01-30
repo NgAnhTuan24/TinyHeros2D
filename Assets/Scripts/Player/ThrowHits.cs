e4e45730cbf932ecc7c47d4267be9de7ef13947f
NgAnhTuan24 <luckytme2004@gmail.com>
2026-01-30 17:30:12 +0700

    Tạo prefab đá (projectile), thêm point spawn đá cho player, dùng animation event để frame ném spawn đá
    Tích hợp thêm đá (projectile) khi nhân vật kích hoạt trạng thái throw thì sẽ spawn viên đá và bay theo hướng ném (1 đường thẳng)

--- Assets/Scripts/Player/ThrowHits.cs
+++ Assets/Scripts/Player/ThrowHits.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class ThrowHits : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+    public float projectileSpeed;
+    public Transform point;
+
     public Animator anim;
 
     void Start()
@@ -20,5 +24,18 @@ public class ThrowHits : MonoBehaviour
     public void GoToBackIdle()
     {
         anim.SetBool("IsThrow", false);
-    } 
+    }
+    
+    public void ThrowStone()
+    {
+        GameObject stone = Instantiate(projectilePrefab, point.position, Quaternion.identity);
+
+        Rigidbody2D rb = stone.GetComponent<Rigidbody2D>();
+
+        if (rb != null )
+        {
+            float dir = transform.localScale.x > 0 ? 1f : -1f;
+            rb.velocity = Vector2.right * dir * projectileSpeed;
+        }
+    }
 }
