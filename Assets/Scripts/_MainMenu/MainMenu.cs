846df5c6e406f26c36943de41ccd6e5118f8e535
NgAnhTuan24 <luckytme2004@gmail.com>
2026-03-08 16:08:42 +0700

    Cập nhật scene MainMenu + Thêm các chức năng và sprite liên quan

--- Assets/Scripts/_MainMenu/MainMenu.cs
+++ Assets/Scripts/_MainMenu/MainMenu.cs
@@ -7,13 +7,4 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(1);
     }
-
-    public void QuitGame()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
-    }
 }
